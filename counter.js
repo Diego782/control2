82fc91c52a7d53fe3c5be00216f593f996054a1f
Diego <diegolara7820@gmail.com>
2024-11-21 07:22:55 -0500

    first commit

---
+++ counter.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+    console.log('Client connected');
+
+    ws.on('message', message => {
+        // message is a Buffer
+        console.log('Received GPS data (raw buffer):', message);
+        console.log('Received GPS data (hex):', message.toString('hex'));
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
