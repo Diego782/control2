a04f4f3096724d63d7d2544d5fd92e276e1dd7a9
Diego <diegolara7820@gmail.com>
2024-11-21 09:17:37 -0500

    fav

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,5 @@ export default defineConfig({
   server: {
     port: 3000, // Especifica el puerto del servidor de desarrollo
   },
+  publicDir: 'public', // Especifica la carpeta de archivos estáticos
 });
