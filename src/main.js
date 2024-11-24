a11c61a80649c7646bcc1fe7f29978f59cb8a105
Diego <diegolara7820@gmail.com>
2024-11-23 22:57:35 -0500

    ad

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // src/main.js
 export function sendCommand(command) {
-    fetch(`http://3.23.99.134/send-command/${command}`)
+    fetch(`http://3.23.134/send-command/${command}`)
         .then(response => response.text())
         .then(data => alert(data))
         .catch(error => console.error('Error:', error));
